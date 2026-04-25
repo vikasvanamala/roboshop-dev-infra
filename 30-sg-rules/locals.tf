c55d89e476fc41e9b2712e259d3fb4bd4bc0f2da
vikas-vanamala <vikasvanamala008@gmail.com>
2026-04-25 19:26:07 +0530

    tf

--- 30-sg-rules/locals.tf
+++ 30-sg-rules/locals.tf
@@ -13,5 +13,5 @@ locals {
   mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
 
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
-   backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
+  backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
 }
--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -28,4 +28,4 @@ else
     cd $ANSIBLE_DIR
 fi
 
-ansible playbook -e component = $component -e env = $environment main.yaml
+ansible playbook -e component=$component -e env=$environment main.yaml
