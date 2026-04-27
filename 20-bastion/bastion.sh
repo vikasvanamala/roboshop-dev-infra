55796a8049c848d8288581ef4e5bb89fabafa802
vikas-vanamala <vikasvanamala008@gmail.com>
2026-04-27 22:16:45 +0530

    terraform

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -12,6 +12,7 @@ sudo yum -y install terraform
 #creating databases
 cd /home/ec2-user
 git clone https://github.com/vikasvanamala/roboshop-dev-infra.git
+# chown ec2-user:ec2-user -R roboshop-dev-infra
 cd roboshop-dev-infra/40-databases
 terraform init 
 terraform apply -auto-approve
--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,8 +3,12 @@ resource "aws_instance" "bastion" {
     instance_type          = var.instance_type
     vpc_security_group_ids = [local.bastion_sg_id]  
     subnet_id = local.public_subnet_id 
-    associate_public_ip_address = true
     iam_instance_profile = aws_iam_instance_profile.bastion.name
+
+    root_block_device {
+        volume_size = 50
+        volume_type = "gp3" # or "gp2", depending on your preference
+    }
     
     # Reads a script named "bastion.sh" in the same directory
   user_data = file("bastion.sh")
