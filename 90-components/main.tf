2cb437a5e0407a804369b48db5bdecfc769a4cba
vikas-vanamala <vikasvanamala008@gmail.com>
2026-04-28 14:17:22 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -6,7 +6,7 @@
 # } 
 
 module "components" {
-    for_each = "var.components"
+    for_each = var.components
     source = "git::https://github.com/vikasvanamala/terraform-roboshop-component.git?ref=main"
     component = each.key
     rule_priority = each.value.rule_priority
